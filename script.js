63bcab176b0df5ce0a197863e65671d60bfd2961
RDVault211 <limzrichie@gmail.com>
2025-05-03 14:57:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ orderForm.onsubmit = async e => {
   alert('Pemesanan berhasil!');
   if (payment_method === 'transfer') {
     const text = `Halo Admin, saya ${buyer_name} ingin memesan ${product_name} untuk ID: ${game_id} ${category === 'Topup ML' ? 'Server ID: ' + server_id : ''}`;
-    const url = `https://wa.me/6281335761181?text=${encodeURIComponent(text)}`;
+    const url = `https://wa.me/6282334077373?text=${encodeURIComponent(text)}`;
     window.open(url, '_blank');
   }
 
